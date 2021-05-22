9fd2b39c864566c3232afa7a0cc00d5aa283c4c3
Benjamin Arntzen <zorlin@gmail.com>
2021-05-22 15:32:08 +0800

    Refactor and cleanup things linter + pycharm were unhappy about

--- director.py
+++ director.py
@@ -1,30 +1,32 @@
 #!/usr/bin/python3
-# "a piece of knowledge, unlike a piece of physical property, can be shared by large groups of people without making anybody poorer."
+# "a piece of knowledge, unlike a piece of physical property,
+# can be shared by large groups of people without making anybody poorer."
 # - Aaron Swartz
 # In loving memory. This is for you.
 
 # Import needed modules
 from __future__ import with_statement
+from pathlib import Path
 import os
 import yaml
 import psycopg2
 import sys
 
 # Define methods
-def fetch_release_rows():
-    print("Building page " + str(pageNum) + " as a group of " + str(page_rows) + " releases.")
-    sql = "FETCH " + str(page_rows) + " FROM director_cur;"
-    cursorpg.execute(sql)
-    result_set = cursorpg.fetchall()
+
 
 def build_page():
+    print("Building page " + str(pageNum) + " as a group of " + str(page_rows) + " releases.")
+    fetchquery = "FETCH " + str(page_rows) + " FROM director_cur;"
+    cursorpg.execute(fetchquery)
+    result_set = cursorpg.fetchall()
     for row in result_set:
         # For every existing release, gather relevant metadata and massage it into Curator.
-        release_id = row[0]
+        release_id = row['id']
         print("DEBUG id: " + str(release_id))
 
+
 # Set our API key
-from pathlib import Path
 apiname = os.path.expanduser('~/.rcc-api')
 apitoken = Path(apiname).read_text()
 
@@ -72,5 +74,12 @@ cursorpg = connpg.cursor()
 
 # Open a read cursor
 # https://www.citusdata.com/blog/2016/03/30/five-ways-to-paginate/
-sql = "DECLARE director_cur CURSOR FOR SELECT * FROM releases ORDER BY id;"
-cursorpg.execute(sql)
+mainquery = "DECLARE director_cur CURSOR FOR SELECT * FROM releases ORDER BY id;"
+cursorpg.execute(mainquery)
+
+# Initiate an empty list
+releases_list = []
+
+# Test
+pageNum = 1
+build_page()
