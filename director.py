04dbfa9f7ed9fbfbcb143245b24c58809b6f87ff
Benjamin Arntzen <zorlin@gmail.com>
2021-05-22 15:10:05 +0800

    Implement working pgsql logic and some basic ideas.

--- director.py
+++ director.py
@@ -2,25 +2,26 @@
 # "a piece of knowledge, unlike a piece of physical property, can be shared by large groups of people without making anybody poorer."
 # - Aaron Swartz
 # In loving memory. This is for you.
-#
-# Credits:
-#  - https://stackoverflow.com/questions/10195139/how-to-retrieve-sql-result-column-value-using-column-name-in-python
-#  - https://github.com/PyMySQL/PyMySQL
 
 # Import needed modules
 from __future__ import with_statement
-from grizzled.os import working_directory
-from urllib.parse import urlparse
-import os, sys, yaml, re
-import importlib
+import os
+import yaml
+import psycopg2
+import sys
 
-# Setup URL parser, thanks https://www.geeksforgeeks.org/python-check-url-string/
-def ParseFindURLs(string):
-    # findall() has been used
-    # with valid conditions for urls in string
-    regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
-    url = re.findall(regex,string)
-    return [x[0] for x in url]
+# Define methods
+def fetch_release_rows():
+    print("Building page " + str(pageNum) + " as a group of " + str(page_rows) + " releases.")
+    sql = "FETCH " + str(page_rows) + " FROM director_cur;"
+    cursorpg.execute(sql)
+    result_set = cursorpg.fetchall()
+
+def build_page():
+    for row in result_set:
+        # For every existing release, gather relevant metadata and massage it into Curator.
+        release_id = row[0]
+        print("DEBUG id: " + str(release_id))
 
 # Set our API key
 from pathlib import Path
@@ -37,78 +38,39 @@ if config is None:
     sys.exit(1338)
 
 # Get our Riff.CC credentials and load them in
-rccuser = config["rccuser"]
-rccpass = config["rccpass"]
 sqlpassword = config["password"]
+curator_user = config["curator_user"]
+curator_pass = config["curator_pass"]
+curator_host = config["curator_host"]
+page_rows = config["page_rows"]
 
-# Login to Riff.CC
-# https://stackoverflow.com/questions/2910221/how-can-i-login-to-a-website-with-python
+# Connect to the Curator database
+connpg = psycopg2.connect(host=curator_host,
+                          database="collection",
+                          user=curator_user,
+                          password=curator_pass)
 
-if(config["login"]):
-    from twill.commands import *
-    go('https://u.riff.cc/login')
+# import data from postgres
 
-    showforms()
-    fv("1", "username", rccuser)
-    fv("1", "password", rccpass)
+# initial setup
 
-    submit('0')
+# verification (optional)
+# shell out to Sentinel and Janitor to validate data
 
-# Set our mysql password
+# build the releases pages
+# build the main releases files
+# build the featured files
+# build the featured categories
+# build the deleted IDs and hashes
 
-import pymysql.cursors
+# pull from the Curator
+# build the related relationships for releases
+# build the related relationships for artists
 
-# Connect to the database
-connection = pymysql.connect(host='localhost',
-                             user='unit3d',
-                             password=sqlpassword,
-                             database='unit3d',
-                             cursorclass=pymysql.cursors.DictCursor)
+# create a cursor
+cursorpg = connpg.cursor()
 
-with connection:
-    with connection.cursor() as cursor:
-        # Read a single record
-        sql = "SELECT `id`, `description` FROM `torrents` WHERE seeders = 0"
-        cursor.execute(sql)
-        result_set = cursor.fetchall()
-        for row in result_set:
-            id = row["id"]
-            print("Processing id "+ str(id))
-            description = row["description"]
-            # Grab our source from the description. Hopefully.
-            # Set up some empty arrays
-            urlLines = []
-            actualURLs = []
-            finalListOfURLs = []
-            for line in description.splitlines():
-                if "Source: [url]" in line:
-                    print(line)
-                    urlLines.append(line)
-            for line in urlLines:
-                # Sanitize the line
-                line = line.replace('[url]', '')
-                line = line.replace('[/url]', '')
-                actualURLs.append(ParseFindURLs(line))
-
-            # Go grab that torrent based on custom rules
-            for actualURL in actualURLs:
-                for actualActualURL in actualURL:
-                    # Find the source of the torrent
-                    sourceDomain = urlparse(actualActualURL).netloc
-                    print(sourceDomain)
-                    # Dynamically load in our magic config files
-                    config = yaml.safe_load(open("rules/" + sourceDomain + ".yml"))
-                    print(config["torrentregex"])
-                    # Jump into the handler for that domain (thanks https://stackoverflow.com/questions/22955684/how-to-import-py-file-from-another-directory)
-                    import importlib.machinery
-
-                    moduleName = sourceDomain.replace('.', '')
-                    loader = importlib.machinery.SourceFileLoader(moduleName, os.getcwd() + '/rules-py/' + moduleName + '.py')
-                    lifehandler = loader.load_module(moduleName)
-
-                    lifehandler.main(actualActualURL)
-
-            # Fetch the torrent
-            #go("https://u.riff.cc/torrents/download/" + str(id))
-            #with working_directory("/tmp/torrents/"):
-                #save_html(str(id) + ".torrent")
+# Open a read cursor
+# https://www.citusdata.com/blog/2016/03/30/five-ways-to-paginate/
+sql = "DECLARE director_cur CURSOR FOR SELECT * FROM releases ORDER BY id;"
+cursorpg.execute(sql)
