1ddd2be0c787d1a2e962fac04c349b1f11c8cdac
Benjamin Arntzen <zorlin@gmail.com>
2021-05-25 21:35:07 +0800

    try to test building and accessing dicts and lists with vars

--- test.py
+++ test.py
@@ -1,11 +1,7 @@
-test_nums = [0, 1]
+test_nums = [
+    0,
+    1,
+    2,
+]
 
-build_tree_dict = {}
-
-for featured_category in test_nums:
-    build_tree_dict["featured_category_data"] = {}
-    build_tree_dict["featured_category_data"][str(featured_category)] = {'hello': 'world'}
-    print(build_tree_dict["featured_category_data"][str(featured_category)])
-    print(str(featured_category))
-    print(str(1))
-    # print(build_tree_dict["featured_category_data"][str(0)])
+print(test_nums)
