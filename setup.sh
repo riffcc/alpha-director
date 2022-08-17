1bcb60787cca0b2c2b40c8b13f27cda8242e5a7a
Benjamin <zorlin@gmail.com>
2022-08-17 21:13:47 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-pip3 install ipfshttpclient
 pip3 install scp
 pip3 install paramiko
-# Optionals
-pip3 install discord.py
-pip3 install discord  # on m1, discord.py does not reliably load :(
